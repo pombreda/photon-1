ac5a5885fc54f3976a6e99353ee2af4f88548255
Ivan Batic <ivan.batic@orangehilldev.com>
2013-10-01 13:16:41 +0200

    Refactored code to make it more readable. Removed return statements in order to comply to the @return void annotation

--- controllers/MigrationGenerator.php
+++ controllers/MigrationGenerator.php
@@ -1,97 +1,82 @@
-<?php namespace Orangehill\Photon;
+<?php
 
-class MigrationGenerator extends \BaseController {
+namespace Orangehill\Photon;
 
-	/**
-	 * Generates a migration that creates a new table
-	 * Original generator CLI command example: php artisan generate:migration create_posts_table --fields="title:string, body:text"
-	 * @param string $table
-	 * @param array $fields
-	 * @return void
-	 */
-	public function create($table, $fields)
-	{
-		$params = array();
-		$params['name'] = 'create_' . $table;
-		$params['--fields'] = $this->concatFields($fields);
+class MigrationGenerator {
 
-		\Artisan::call('generate:migration', $params);
-	}
+    /**
+     * Generates a migration that creates a new table
+     * Original generator CLI command example: php artisan generate:migration create_posts_table --fields="title:string, body:text"
+     * @param string $table
+     * @param array $fields
+     * @return void
+     */
+    public function create($table, $fields) {
+        $params = array(
+            'name'     => 'create_' . $table,
+            '--fields' => $this->concatFields($fields)
+        );
+        \Artisan::call('generate:migration', $params);
+    }
 
-	/**
-	 * Generates a migration that adds a field to existing table
-	 * Original generator CLI command example: php artisan generate:migration add_user_id_to_posts_table --fields="title:string, body:text"
-	 * @param string $table
-	 * @param array $fields
-	 * @return void
-	 */
-	public function add($table, $fields)
-	{
-		if (!is_array($fields)) return false;
-		$params = array();
-		$params['name'] = 'add_' . key($fields) . '_to_' . $table . '_table';
-		$params['--fields'] = $this->concatFields($fields);
+    /**
+     * Generates a migration that adds a field to existing table
+     * Original generator CLI command example: php artisan generate:migration add_user_id_to_posts_table --fields="title:string, body:text"
+     * @param string $table
+     * @param array $fields
+     * @return void
+     */
+    public function add($table, array $fields) {
+        $key = key($fields);
+        $params = array(
+            'name'     => "add_{$key}_to_{$table}_table",
+            '--fields' => $this->concatFields($fields)
+        );
+        \Artisan::call('generate:migration', $params);
+    }
 
-		return \Artisan::call('generate:migration', $params);
+    /**
+     * Generates a migration that removes a field from existing table
+     * Original generator CLI command example: php artisan generate:migration remove_user_id_from_posts_table --fields="title:string, body:text"
+     * @param string $table
+     * @param array $fields
+     * @return void
+     */
+    public function remove($table, array $fields) {
+        $key = key($fields);
+        $params = array(
+            'name'     => "remove_{$key}_from_{$table}_table",
+            '--fields' => $this->concatFields($fields)
+        );
+        \Artisan::call('generate:migration', $params);
+    }
 
-	}
+    /**
+     * Generates a migration that destroys a table
+     * Original generator CLI command example: php artisan generate:migration destroy_posts_table --fields="title:string, body:text"
+     * @param string $table
+     * @param array $fields
+     * @return void
+     */
+    public function destroy($table, array $fields) {
+        $params = array(
+            'name'     => "destroy_{$table}",
+            '--fields' => $this->concatFields($fields)
+        );
+        \Artisan::call('generate:migration', $params);
+    }
 
-	/**
-	 * Generates a migration that removes a field from existing table
-	 * Original generator CLI command example: php artisan generate:migration remove_user_id_from_posts_table --fields="title:string, body:text"
-	 * @param string $table
-	 * @param array $fields
-	 * @return void
-	 */
-	public function remove($table, $fields)
-	{
-		if (!is_array($fields)) return false;
-		$params = array();
-		$params['name'] = 'remove_' . key($fields) . '_from_' . $table . '_table';
-		$params['--fields'] = $this->concatFields($fields);
-
-
-		return \Artisan::call('generate:migration', $params);
-
-	}
-
-	/**
-	 * Generates a migration that destroys a table
-	 * Original generator CLI command example: php artisan generate:migration destroy_posts_table --fields="title:string, body:text"
-	 * @param string $table
-	 * @param array $fields
-	 * @return void
-	 */
-	public function destroy($table, $fields)
-	{
-		if (!is_array($fields)) return false;
-		$params = array();
-		$params['name'] = 'destroy_' . $table;
-		$params['--fields'] = $this->concatFields($fields);
-
-		return \Artisan::call('generate:migration', $params);
-
-	}
-
-	/**
-	 * Returns a string formatted as e.g. 'key0:val0, key1:val1'
-	 * @param  array $array
-	 * @return string
-	 */
-	protected function concatFields($array)
-	{
-		if(is_array($array))
-		{
-
-			$output = array();
-
-			foreach($array as $key=>$val)
-			{
-				$output[] = $key . ':' . $val;
-			}
-			return implode(', ', $output);
-		}
-		return false;
-	}
+    /**
+     * Returns a string formatted as e.g. 'key0:val0, key1:val1'
+     * @param  array $array
+     * @return string
+     */
+    protected function concatFields(array $array) {
+        $output = array();
+        foreach ($array as $key => $val) {
+            $output[] = $key . ':' . $val;
+        }
+        return implode(', ', $output);
+    }
 
 }
